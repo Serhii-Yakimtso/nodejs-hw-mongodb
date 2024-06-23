c78ac982529fee6898e6058dd7a69c186dcf892f
Serhii-Yakimtso <serafyso@gmail.com>
2024-06-24 00:20:51 +0300

    edit pino

--- src/server.js
+++ src/server.js
@@ -7,17 +7,15 @@ const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  });
 
   app.use(express.json());
   app.use(cors());
-
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  app.use(logger);
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
