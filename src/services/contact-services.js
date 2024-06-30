c60861e67ec0d0e77ac40ddefbb2fb74d1d1e924
Serhii-Yakimtso <serafyso@gmail.com>
2024-06-30 20:19:12 +0300

    edit

--- src/services/contact-services.js
+++ src/services/contact-services.js
@@ -1,4 +1,4 @@
-import { Contact } from '../db/contact';
+import { Contact } from '../db/contact.js';
 
 export const getContacts = () => Contact.find();
 export const getContactById = (id) => Contact.findById(id);
